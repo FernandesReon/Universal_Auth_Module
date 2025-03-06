3ee7feb628c167777fd36f9a346fd53d0d629169
Reon Fernandes <reondias@gmail.com>
2025-03-06 20:26:40 +0530

    Database configured
    mysql and redis

---
+++ src/main/java/com/reon/Universal_Auth_Module/UniversalAuthModuleApplication.java
@@ -0,0 +1,13 @@
+package com.reon.Universal_Auth_Module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalAuthModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversalAuthModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reon/Universal_Auth_Module/UniversalAuthModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reon.Universal_Auth_Module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalAuthModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
